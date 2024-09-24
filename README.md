47fb86c4b3d440859d599b03a6d83da94d142359
pslcorrea <pslcorrea@gmail.com>
2024-09-24 19:50:39 -0300

    criação de novos endpoint e atualização do README

---
+++ README.md
@@ -0,0 +1,3 @@
+## Estudo de node
+
+> o aprendizado de Node JS está sendo fantástico
--- app.mjs
+++ app.mjs
@@ -1,18 +1,28 @@
 import http from "node:http";
 import { stock } from "./stock.mjs";
+import { URL } from "node:url";
 
 const server = http.createServer();
 
 server.addListener("request", (request, response) => {
-  if (request.url === "/") {
+  const urlObject = new URL(`http://${request.headers.host}${request.url}`);
+  if (urlObject.pathname === "/") {
     response.writeHead(200, { "Content-Type": "application/json" });
     response.write(JSON.stringify(stock));
     response.end();
   }
-  if (request.url === "/get-missing-products") {
+  if (urlObject.pathname === "/get-missing-products") {
     const missingProducts = stock.filter((product) => product.amountLeft === 0);
     response.writeHead(200, { "Content-Type": "application/json" });
     response.write(JSON.stringify(missingProducts));
+    response.end();
+  }
+  if (urlObject.pathname === "/get-by-id") {
+    const selectedObject = stock.find(
+      (product) => product.id === Number(urlObject.searchParams.get("id"))
+    );
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(selectedObject));
     response.end();
   }
 });
