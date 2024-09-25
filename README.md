b8175ef8fa242444867398f1660cd82e9473038c
pslcorrea <pslcorrea@gmail.com>
2024-09-25 17:20:54 -0300

    Tratamento de erros

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## Estudo de node
 
 > o aprendizado de Node JS está sendo fantástico
+
+### Métodos HTTP
+
+#### GET
+
+Requisicão para um endereço
+
+#### POST
+
+#### PUT
+
+#### DELETE
--- app.mjs
+++ app.mjs
@@ -18,12 +18,26 @@ server.addListener("request", (request, response) => {
     response.end();
   }
   if (urlObject.pathname === "/get-by-id") {
+    const idParam = urlObject.searchParams.get("id");
+    if (!idParam || isNaN(idParam)) {
+      response.writeHead(400, { "Content-Type": "text/plain" });
+      response.write("Informe um ID númerico!");
+      response.end();
+      return;
+    }
     const selectedObject = stock.find(
-      (product) => product.id === Number(urlObject.searchParams.get("id"))
+      (product) => product.id === Number(idParam)
     );
+    if (!selectedObject) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("Não existe um produto com esse ID!");
+      response.end();
+      return;
+    }
     response.writeHead(200, { "Content-Type": "application/json" });
     response.write(JSON.stringify(selectedObject));
     response.end();
+    return;
   }
 });
 
